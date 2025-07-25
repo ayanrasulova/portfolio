e3bf7fdc35d1a82f69a6ad6968b899390f80584f
ion <ayanrasulova13@gmail.com>
2025-07-25 03:59:57 -0400

    got rid of left side spotlight and tried making effect bigger

--- src/components/ui/spotlight-new.tsx
+++ src/components/ui/spotlight-new.tsx
@@ -19,8 +19,8 @@ export const Spotlight = ({
   gradientSecond = "radial-gradient(50% 50% at 50% 50%, hsla(210, 100%, 85%, .06) 0, hsla(210, 100%, 55%, .02) 80%, transparent 100%)",
   gradientThird = "radial-gradient(50% 50% at 50% 50%, hsla(210, 100%, 85%, .04) 0, hsla(210, 100%, 45%, .02) 80%, transparent 100%)",
   translateY = -350,
-  width = 560,
-  height = 1380,
+  width = 700, //originally 560
+  height = 1600, // og 1380
   smallWidth = 240,
   duration = 7,
   xOffset = 100,
@@ -38,6 +38,9 @@ export const Spotlight = ({
       }}
       className="pointer-events-none absolute inset-0 h-full w-full"
     >
+      
+      
+      {/* LEFT SIDE SPOTLIGHT
       <motion.div
         animate={{
           x: [0, xOffset, 0],
@@ -79,7 +82,9 @@ export const Spotlight = ({
           }}
           className={`absolute top-0 left-0 origin-top-left`}
         />
-      </motion.div>
+      </motion.div>  
+      */}
+
 
       <motion.div
         animate={{
