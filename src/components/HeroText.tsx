7f84e8203e250d5d92f1e01f0741eb4bdcf2971d
ion <ayanrasulova13@gmail.com>
2025-07-25 21:24:30 -0400

    tried adding grunge stuff but will honestly do for art website cuz no way bruh

--- src/components/HeroText.tsx
+++ src/components/HeroText.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {TypewriterEffect} from "./TypewriterEffect";
 import { motion } from "motion/react";
+import BuzzEffect from "./ui/buzz";
 
 /* const words = [ // words for typewriter effect
     { text: "passionate"},
@@ -41,27 +42,28 @@ import { motion } from "motion/react";
   
   
 const HeroText = () => {
-    return ( // added pt-30 for space from top, going to try to change the font size 
-        <div className="z-10 mt-20 text-center md:mt-40 md:text-left rounded-3xl bg-clip-text pl-0 ml-0"> 
+    return ( // added pt-30 for space from top, going to try to change the font size             
+        <div className="z-10 mt-20 md:mt-40 items-start md:text-left rounded-3xl bg-clip-text pl-0 ml-0"> 
 
-        {/* <div className="z-10 mt-20 text-center md:mt-40 md:text-left rounded-3xl bg-clip-text "> */}
 
             {/* --------------- desktop, hidden, but shown for desktop --------------- */}
-            <div className = "flex-col justify-start hidden md:flex md:px-15 lg:px-15 "> {/* not using c-space so left aligned, justify start + padding on larger screens */}
-                <h1 className = "text-4xl text-neutral-300" >front-end software developer. <br /> computer science @ uva</h1>
+            <div className = "flex-col justify-start items-start hidden md:flex md:px-15 lg:px-15 "> {/* not using c-space so left aligned, justify start + padding on larger screens */}
+                <h1 className = "text-4xl pb-2 text-neutral-300" >front-end software developer. <br /> computer science @ uva</h1>
+
                 <div className = "flex flex-col items-start"> 
-                    {/* <p className = "text-sm text-neutral-300"> passionate about ui/ux, visual design, and the language that guides user experience </p> */}
+                    {/*used to be items-start */}
                     
                      <div>
                          <TypewriterEffect words={wordsLine1} className="text-2xl text-neutral-400" continue/>
                          <br />
                          <TypewriterEffect words={wordsLine2} className="text-2xl text-neutral-400" delay = {2.7}/>
                     </div>
-                    <div> {/* wanted to add projects on hero page */}
+                    <div> 
                         <button className = "text-sm  hover:bg-neutral-300 hover:text-navbar text-neutral-300 rounded shadow p-1.5 mt-4"> see my projects </button>
                     </div>                    
                 </div>
             </div>
+
 
             {/*--------------- mobile ---------------*/}
             <div className ="flex flex-col items-center text-center mt-10 space-y-0.3 px-3 pr-10 pl-10 md:hidden"> {/* c-space */}
