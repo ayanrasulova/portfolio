26b71345282501cc3b9927d47b4d5777a78ebf0f
ion <ayanrasulova13@gmail.com>
2026-05-09 20:57:27 -0400

    fixed grid gallery

--- src/pages/CreativeWork.tsx
+++ src/pages/CreativeWork.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "../sections/Navbar";
 import Footer from "../sections/Footer";
+import CreativeGrid from "../sections/CreativeGrid";
 
 //function App() { // self closing syntax
  // return <div><Message /></div>
@@ -12,6 +13,10 @@ const CreativeWork = () => {
     <div className = "container mx-auto max-w-7xl">
         <div className = "z-30"><Navbar /> </div>
         <div className = "z-30"><Footer /> </div>
+
+        <section className = "justify-center align-center items-center flex flex-col overscroll-none noscroll">
+          <div className = "z-10"> <CreativeGrid /> </div>
+        </section>
     </div>
   )
 }
--- src/sections/CreativeGrid.tsx
+++ src/sections/CreativeGrid.tsx
@@ -1,13 +1,45 @@
-import React from "react";
-// import { motion } from "motion/react";
+import { useState, type FC } from "react";
 
+type Panel = {
+  image: string;
+};
 
-const CreativeGrid = () => {
-    return ( // added pt-30 for space from top, going to try to change the font size             
-        <div className="w-[1280px] h-[80v] flex items-center justify-center"> 
+const panels: Panel[] = [
+  { image: "src/assets/creativeworkimages/singleusefitzshow.jpg" },
+  { image: "src/assets/creativeworkimages/valentines2026green.jpg" },
+  { image: "src/assets/creativeworkimages/indieheadsimg.jpg" },
+  { image: "src/assets/creativeworkimages/indieheads2.jpg" },
+  { image: "src/assets/creativeworkimages/indieheadscakeonlawn.jpg" },
+  { image: "src/assets/creativeworkimages/silenthill.jpg" },
+  { image: "src/assets/creativeworkimages/v2.jpg" },
+];
+
+const CreativeGrid: FC = () => {
+  const [expandedIndex, setExpandedIndex] = useState<number>(0);
+
+  const handleClick = (index: number) => {
+    setExpandedIndex(index);
+  };
+
+  return (
+    <main className="w-screen h-screen overflow-hidden flex items-center justify-center">
+      <div className="flex max-w-7xl h-[80vh] gap-2 items-center justify-center content-center z-10 mx-auto">
+            
+          {panels.map((panel, index) => (
+            <div
+              key={index}
+              onClick={() => handleClick(index)}
+              className={`h-[80vh] rounded-2xl bg-black cursor-pointer transition-all duration-500 ease-in-out overflow-hidden ${
+                expandedIndex === index ? "w-[60%]" : "w-[10%] hover:bg-gray-200"
+              } min-w-[40px] block`}
+            >
+              <img loading="lazy" src={panel.image} alt={`panel-${index}`} className="w-full h-full  object-cover object-top" style={{ maxWidth: expandedIndex === index ? "100%" : "200px" }} />
+            </div>
+          ))}
 
         </div>
-    )
-}
+    </main>
+  );
+};
 
 export default CreativeGrid
