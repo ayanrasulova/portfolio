51901ee24cda230bac333d7fdf17dd5cc3ca5188
ion <ayanrasulova13@gmail.com>
2026-05-21 17:27:45 -0400

    update hero

--- src/components/HeroText.tsx
+++ src/components/HeroText.tsx
@@ -36,18 +36,15 @@ const HeroText = () => {
         <div className="z-10 mt-15 md:mt-32 items-start md:text-left rounded-3xl bg-clip-text pl-0 ml-0"> 
 
 
-            {/* --------------- desktop, hidden, but shown for desktop  --------------- */}
+            {/* desktop */}
             <div className = "mt-50 flex-col justify-center hidden items-center md:flex md:px-15 lg:px-15 "> {/* not using c-space so left aligned, justify start + padding on larger screens */}
   
                 <FuzzyText 
                     baseIntensity={0.06}
                     enableHover={false}
                     >
-                    hi, i'm ion
+                    hi, i'm ayan
                 </FuzzyText>
-
-                {/* <h1 className = "text-9xl text-neutral-300"> hi, i'm ion </h1> */}
-                {/* <h1 className = "text-3xl text-neutral-300"> (or ayan) </h1> */}
 
                 <div className = "flex flex-col items-center"> 
                     
@@ -67,7 +64,7 @@ const HeroText = () => {
             </div>
 
 
-            {/*--------------- mobile ---------------*/}
+            {/* mobile */}
             <div className ="flex flex-col items-center text-center mt-10 space-y-0.3 px-3 pr-10 pl-10 md:hidden"> {/* c-space */}
                 <FuzzyText className = "mt-50"
                     baseIntensity={0.06}
@@ -77,7 +74,6 @@ const HeroText = () => {
                 </FuzzyText>
 
                 <div className = "flex flex-col pt-3 items-center"> 
-                    {/* <p className = "text-sm text-neutral-300"> passionate about ui/ux, visual design, and the language that guides user experience </p> */}
 
                     <div className = "items-center mt-2">
                          <TypewriterEffect words={wordsLine1} className=" text-neutral-400" continue/>
@@ -88,7 +84,6 @@ const HeroText = () => {
                     </div>
 
                     <div className = " text-sm  pt-3 mt-3 "> 
-                        {/* <button className = "text-sm  hover:bg-neutral-300 hover:text-navbar text-neutral-300 rounded shadow p-1.5 mt-4"> see my projects </button> */}
                         <a href = "/projects"> <InteractiveHoverButton>see my projects</InteractiveHoverButton> </a> 
                     </div>   
 
